aebd14fac44ee72211f28a3f85cc479f418a1ead
sadko4u <sadko4u@gmail.com>
2022-01-16 01:40:47 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-art-delay.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_ART_DELAY
-ARTIFACT_NAME               = lsp-plugins-art-delay
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Artistic Delay Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_ART_DELAY
+PLUGIN_PACKAGE_NAME     = lsp-plugins
