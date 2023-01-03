1410c8b927b6915879e123ebfa472e6229f280e9
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:48:03 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-art-delay
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Artistic Delay Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.7-devel
 
 
